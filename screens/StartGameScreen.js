5e4bcad3c91503bda419efe18db4d49c95d85065
cyrusonline <online_mad@yahoo.com.hk>
2019-09-14 16:40:12 +0800

    configuring and styling a textinput

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,51 +1,66 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
-import Card from '../components/Card'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Card from "../components/Card";
+import Input from "../components/Input";
+import Colors from "../constants/colors";
 const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a new</Text>
-            
-                
-            <Card style={styles.inputContainer}>
-                <Text>Select a number</Text>
-                <TextInput/>
-                <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={()=>{}}/>
-                    <Button title="Confirm" onPress={()=>{}}/>
-                </View>
-            </Card>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new</Text>
+
+      <Card style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Confirms"
+              onPress={() => {}}
+              color={Colors.primary}
+            />
+          </View>
         </View>
-    )
+      </Card>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    screen:{
-       flex:1,
-        padding:10,
-        alignItems:'center'
-    },
-    title:{
-        fontSize:20,
-        marginVertical:10
-    },
-    inputContainer:{
-        width:300,
-        maxWidth:'80%',
-        alignItems:'center',
-        shadowColor: 'black',
-        shadowOffset:{width:0, height:2},
-        shadowRadius:6,
-        shadowOpacity:0.26,
-        elevation:5,
-        backgroundColor:'white',
-        padding:20,
-        borderRadius:10
-    },
-    buttonContainer:{
-        flexDirection: 'row',
-        width:'100%',
-        justifyContent:'space-between',
-        paddingHorizontal: 15
-    }
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15
+  },
+  button: {
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: "center"
+  }
 });
-export default StartGameScreen
+export default StartGameScreen;
