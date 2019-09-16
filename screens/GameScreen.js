163eef6334dbadf7f9a38c1fac3b86e6f7cdf87b
cyrusonline <online_mad@yahoo.com.hk>
2019-09-16 22:45:39 +0800

    add min

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 const generateRandomBetween = (min, max, exclude) =>{
     min = Math.ceil(min);
     max = Math.floor(max)
-    const rndNum = Math.floor(Math.random()*(max-min))+;
+    const rndNum = Math.floor(Math.random()*(max-min))+min; 
     if (rndNum == exclude){
         return generateRandomBetween(min, max, exclude)
     } else {
